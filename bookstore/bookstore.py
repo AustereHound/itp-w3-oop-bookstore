c6188a48587c44b1ccd850f02b3a156d0501e90a
Dan O <dan47573@gmail.com>
2017-04-08 17:55:12 +0000

    OOP Bookstore Dan O

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,39 @@
 class Bookstore(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.library = []
+    def get_books(self):
+        return self.library
+    def add_book(self, book):
+        self.library.append(book)
+    def search_books(self, title = None, author = None):
+        matches = []
+        for book in self.library:
+            if title:
+                if title in book.title.lower():
+                    matches.append(book)
+                if title in book.title:
+                    matches.append(book)
+            if author:
+                if author.name in book.author.name.lower():
+                    matches.append(book)
+                if author.name in book.author.name:
+                    matches.append(book)
+        return matches
+            
 
 class Author(object):
-    pass
-
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.library = []
+    def get_books(self):
+        return self.library
 
 class Book(object):
-    pass
+    def __init__(self, title, author = None):
+        self.title = title
+        if author:
+            self.author = author
+            author.library.append(self)
+        
